088208a32d7685dc343109361c4b2da4fbe66a1f
widyanirwadi <widyan.social@gmail.com>
2023-06-11 10:26:42 +0700

    Add new class

---
+++ Class/LearnClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    public class LearnClass
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public void Introduce()
+        {
+            Console.WriteLine("Hello my name is {0} {1}", FirstName, LastName);
+        }
+
+    }
+}
